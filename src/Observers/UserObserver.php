d26ccc67a2132d3b718fd7dbdcc35c60210a9ea7
Jan Bladt <jan.bladt@syspons.com>
2021-09-06 11:48:04 +0200

    * add deleting and updating keycloak users * set a keycloak password from outside

--- src/Observers/UserObserver.php
+++ src/Observers/UserObserver.php
@@ -5,7 +5,7 @@ namespace berthott\KeycloakUsers\Observers;
 use Closure;
 use berthott\KeycloakUsers\Exceptions\KeycloakUsersException;
 use berthott\KeycloakUsers\Models\User;
-use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 use Mnikoei\Facades\KeycloakAdmin;
 
 class UserObserver
@@ -22,12 +22,22 @@ class UserObserver
     {
       $user->username = "$user->firstName.$user->lastName";
       $this->captureExceptions(function () use ($user) {
+        $password = 'avocado123';
         $createdUser = KeycloakAdmin::user()->create([
           'body' => [  // https://www.keycloak.org/docs-api/14.0/rest-api/index.html#_userrepresentation
             'username' => $user->username,
             'firstName' => $user->firstName,
             'lastName' => $user->lastName,
             'email' => $user->email,
+            'enabled' => true,
+          ]
+        ]);
+        KeycloakAdmin::user()->setTemporaryPassword([
+          'id' => $createdUser['id'],
+          'body' => [  // https://www.keycloak.org/docs-api/14.0/rest-api/index.html#_userrepresentation
+            'type' => 'password',
+            'value' => 'avocado123',
+            'temporary' => true,
           ]
         ]);
         $user->id = $createdUser['id'];
@@ -42,29 +52,34 @@ class UserObserver
      */
     public function updated(User $user)
     {
-        //
+      $user->username = "$user->firstName.$user->lastName";
+      $this->captureExceptions(function () use ($user) {
+        KeycloakAdmin::user()->update([
+          'id' => $user->id,
+          'body' => [  // https://www.keycloak.org/docs-api/14.0/rest-api/index.html#_userrepresentation
+            'username' => $user->username,
+            'firstName' => $user->firstName,
+            'lastName' => $user->lastName,
+            'email' => $user->email,
+          ]
+        ]);
+      });
     }
 
     /**
      * Handle the User "deleted" event.
+     * Delete the user inside Keycloak.
      *
      * @param  \App\Models\User  $user
      * @return void
      */
     public function deleted(User $user)
     {
-        //
-    }
-
-    /**
-     * Handle the User "forceDeleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function forceDeleted(User $user)
-    {
-        //
+      $this->captureExceptions(function () use ($user) {
+        KeycloakAdmin::user()->delete([
+          'id' => $user->id
+        ]);
+      });
     }
 
     /**
@@ -77,7 +92,7 @@ class UserObserver
     {
         try {
           $f();
-        } catch (ClientException $e) {
+        } catch (RequestException $e) {
           throw new KeycloakUsersException($e);
         }
     }
